8371ff6b84e3071656d5fb68f0fd0560f1377ed3
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-10-20 19:26:58 +0400

    Array Minimum Maximum Solution

---
+++ ArrayMinMax/ArrayMinMax/Array.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMinMax
+{
+    class Array
+    {
+        private int[] _array = new int[6]; 
+        public int this[int index]
+        {
+            set
+            {
+                _array[index] = value;
+            }
+            get
+            {
+                return _array[index];
+            }
+            
+        }
+        public void MinMax()
+        {
+            int max = _array[0];
+            for (int index = 0; index < _array.Length; index++)
+            {
+                if (_array[index] > max)
+                    max = _array[index];
+            }
+            Console.WriteLine($"The Maximum Element is`{max}");
+            int min = _array[0];
+            for (int index = 0; index <_array.Length; index++)
+            {
+                if (_array[index] < min)
+                    min = _array[index];
+            }
+            Console.WriteLine($"The Minimum element is` {min}");
+        }
+        public void Sum()
+        {
+            int sum = 0;
+            for (int index = 0; index < _array.Length; index++)
+            {
+                sum += _array[index];
+            }
+            Console.WriteLine($"Array Sum`{sum}");
+        }
+        public void ArithmeticElements()
+        {
+            int AritEL = 0;
+            for (int index = 0; index < _array.Length; index++)
+            {
+                AritEL += _array[index];
+            }
+            Console.WriteLine($"Arithmetic mean of all elements is`{AritEL/2}");
+        }
+        public void OddValue()
+        {
+            for (int index = 0; index < _array.Length; index++)
+            {
+                if(_array[index]%2!=0)
+                    Console.WriteLine($"All ODD Values`{_array[index]}");
+            }
+        }
+    }
+}
---
+++ ArrayMinMax/ArrayMinMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            Console.WriteLine("Please type  Number`");
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type next  Number`");
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type next Number`");
+            array[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type next Number`");
+            array[3] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type next Number`");
+            array[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type next Number`");
+            array[5] = Convert.ToInt32(Console.ReadLine());
+            array.MinMax();
+            array.Sum();
+            array.ArithmeticElements();
+            array.OddValue();
+        }
+    }
+}
