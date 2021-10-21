f694b113d12e6415380bbd95e1a31d83b27fed09
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-10-21 17:54:21 +0400

    Shop Products

--- Product in Shop/Product in Shop/Program.cs
+++ Product in Shop/Product in Shop/Program.cs
@@ -16,11 +16,16 @@ namespace Product_in_Shop
             article.VegetablesPrice = 2;
             Store store = new Store();
             store[0] = article.Cheese;
-            //store[1] = article._meat;
-            //store[2] = article._vegetables;
-            store.Method();
-            
-
+            store[1] = article.Meat;
+            store[2] = article.Vegetables;
+            store.IndexCall(article.CheesePrice, article.MeatPrice, article.VegetablesPrice);
+            article.Cheese = "Cheese";
+            article.Meat = "Meat";
+            article.Vegetables = "Vegetable";
+            store[0] = article.Cheese;
+            store[1] = article.Meat;
+            store[2] = article.Vegetables;
+            store.NameCall();
         }
     }
 }
--- Product in Shop/Product in Shop/Store.cs
+++ Product in Shop/Product in Shop/Store.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Product_in_Shop
 {
-    class Store 
+    class Store
     {
         private string[] _store = new string[3];
         public string this[int index]
@@ -20,19 +20,29 @@ namespace Product_in_Shop
                 _store[index] = value;
             }
         }
-        public void Method()
+        public void IndexCall(int cheese, int meat, int vegetable)
         {
             Console.WriteLine("Please Type index of Product ` 1- 3");
             int productIndex = Convert.ToInt32(Console.ReadLine());
-            if(productIndex<=_store.Length)
-                Console.WriteLine(_store[productIndex]);
+            if (productIndex <= _store.Length)
+                Console.WriteLine($"Product name`{_store[productIndex]}");
+            int[] price = new int[] { cheese, meat, vegetable };
+                for (int i = 0; i < price.Length; i++)
+                {
+                    if (productIndex == i)
+                        Console.WriteLine($"Product Price`{price[0]}");
+                }
+        }
+        public void NameCall()
+        {
             Console.WriteLine("Please Type Name of Product");
             string productName = Console.ReadLine();
             for (int i = 0; i < _store.Length; i++)
             {
-                if(productName == _store[i])
-                    Console.WriteLine(_store[i]);
+                if (productName == _store[i])
+                    Console.WriteLine($"We have that product name`{_store[i]}");
             }
         }
+
     }
 }
