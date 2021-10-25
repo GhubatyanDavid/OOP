077a35b7b84ad23f6cd95b1d7d818ede39eb5c77
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-10-25 21:54:23 +0400

    Fake Notepad

---
+++ BookNotesSave/BookNotesSave/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNotesSave
+{
+    class Book
+    {
+        public string UserNotes(string notes)
+        {
+            Notes notes1 = new Notes();
+            return notes1.UserNotes(notes);
+        }
+        class Notes
+        {
+            public string UserNotes(string notes)
+            {
+                Console.WriteLine("Your Note Saved!");
+                Console.WriteLine("If you want see type 1 if not type 2");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                string userNotes = notes;
+                if(userNumber == 1)
+                    Console.WriteLine(notes);
+                else
+                    Console.WriteLine("Bye!!");
+                return userNotes;
+            }
+        }
+    }
+}
---
+++ BookNotesSave/BookNotesSave/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BookNotesSave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type anything you want in your note :)");
+            string userType = Console.ReadLine();
+            Book book = new Book();
+            book.UserNotes(userType);
+        }
+    }
+}
