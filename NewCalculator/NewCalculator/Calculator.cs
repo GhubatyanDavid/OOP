6fcbcffaefcf99b94ac28dd3f93b72220ee7e9ee
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-10-25 20:11:00 +0400

    New Calculator with OOP

---
+++ NewCalculator/NewCalculator/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCalculator
+{
+    static class Calculator
+    {
+        private static int _firstNumber;
+        private static int _secondNumber;
+        private static string _arithmeticOperation;
+        public static int FirstNumber
+        {
+            get { return _firstNumber; }
+            set { _firstNumber = value; }
+        }
+        public static int SecondNumber
+        {
+            get { return _secondNumber; }
+            set { _secondNumber = value; }
+        }
+        public static string ArithmeticOperation
+        {
+            get { return _arithmeticOperation; }
+            set { _arithmeticOperation = value; }
+        }
+        public static void DeterminedOperation()
+        {
+             if (_arithmeticOperation == "+")
+            {
+                int sum = _firstNumber + _secondNumber;
+                Console.WriteLine($"{_firstNumber} + {_secondNumber} = {sum}");
+            }
+            else if (_arithmeticOperation == "-" )
+            {
+                int sum = _firstNumber - _secondNumber;
+                Console.WriteLine($"{_firstNumber} - {_secondNumber} = {sum}");
+            }
+            else if (_arithmeticOperation == "/" )
+            {
+                if (_secondNumber == 0)
+                {
+                    Console.WriteLine("The number is not divisible by zero");
+                }
+                else
+                {
+                    int sum = _firstNumber + _secondNumber;
+                    Console.WriteLine($"{_firstNumber} / {_secondNumber} = {sum}");
+                }
+            }
+            else if (_arithmeticOperation == "*") 
+            {
+                int sum = _firstNumber + _secondNumber;
+                Console.WriteLine($"{_firstNumber} * {_secondNumber} = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Error 404! Please Try again!");
+            }
+        }
+    }
+}
---
+++ NewCalculator/NewCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NewCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Type First Number`");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Type Second Number`");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Type The Arithmetic operation`'+' '-' '/' '*'");
+            string arithmeticOperation = Console.ReadLine();
+            Calculator.FirstNumber=firstNumber;
+            Calculator.SecondNumber = secondNumber;
+            Calculator.ArithmeticOperation = arithmeticOperation;
+            Calculator.DeterminedOperation();
+        }
+    }
+}
