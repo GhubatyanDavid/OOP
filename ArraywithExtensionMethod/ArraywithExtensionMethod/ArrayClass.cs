af54d60f8d4a4d0078c6f02981c055f924659098
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-10-26 20:53:01 +0400

    Numbers from small to large

---
+++ ArraywithExtensionMethod/ArraywithExtensionMethod/ArrayClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraywithExtensionMethod
+{
+    static class ArrayClass
+    {
+        public static int Method(this int[] value)
+        {
+            int[] array = value;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minNumber = array[i];
+                for (int j = 1; j < array.Length; j++)
+                {
+                    if (minNumber < array[j])
+                    {
+                        minNumber = array[j];
+                        array[j] = array[i];
+                        array[i] = minNumber;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            return array[5];
+        }
+    }
+}
---
+++ ArraywithExtensionMethod/ArraywithExtensionMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArraywithExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 1, 8, 9, 6, 2 };
+            ArrayClass.Method(array);
+        }
+    }
+}
