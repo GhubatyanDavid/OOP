fced233483d1e9f47c8aed51b9ee81186ca6a1f0
David Ghubatyan <ghubatyandavid@gmail.com>
2021-11-08 12:44:07 +0400

    String Color Change

---
+++ StringColor/StringColor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = Enum.GetValues(typeof(EnumType));
+            Console.WriteLine("Please Type the Line of words`");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Please type Color of Line(example` White,Yellow,Blue,Red)");
+            int colorNumber =Convert.ToInt32(Console.ReadLine());
+            object color = null ;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (colorNumber == i)
+                {
+                    color = array.GetValue(i);
+                    break;
+                }
+            }
+            String colorName = ((EnumType)color).ToString();
+            PrintString.Print(stroka, colorName);
+            
+        }
+    }
+}
