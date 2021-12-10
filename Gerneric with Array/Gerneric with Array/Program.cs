77e6166ebcfcd57b6e2af7a5ec77a4e378491615
David Ghubatyan <ghubatyandavid@gmail.com>
2021-12-10 22:49:57 +0400

    Array with Generic

--- Gerneric with Array/Gerneric with Array/Program.cs
+++ Gerneric with Array/Gerneric with Array/Program.cs
@@ -5,6 +5,17 @@ namespace Gerneric_with_Array
     class MyClass<T>
     {
         public T[] array = new T[4];
+        public MyClass()
+        {
+           T[] array = new T[4];
+        }
+        public MyClass(params T[] array )
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
         public T this[int  index]
         {
             get
@@ -16,6 +27,7 @@ namespace Gerneric_with_Array
                 array[index] = value;
             }
         }
+        
         public void Add(T[] array,T number)
         {
             int i = 0;
@@ -33,21 +45,22 @@ namespace Gerneric_with_Array
         static void Main(string[] args)
         {
             MyClass<int> myClass = new MyClass<int>();
-            Console.WriteLine("Please Type First Number for array");
-            myClass[0] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Type Second Number for array");
-            myClass[1] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Type Third Number for array");
-            myClass[2] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Type Four Number for array");
-            myClass[3] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please type final Number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            for (int index = 0; index < myClass.array.Length; index++)
-            {
-                Console.WriteLine($"{index}-st Array is `{myClass.array[index]}");
-            }
-            myClass.Add(myClass.array, number);
+            MyClass<int> myClass1 = new MyClass<int>(4,2,6,8);
+            //Console.WriteLine("Please Type First Number for array");
+            //myClass[0] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Type Second Number for array");
+            //myClass[1] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Type Third Number for array");
+            //myClass[2] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please Type Four Number for array");
+            //myClass[3] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please type final Number");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //for (int index = 0; index < myClass.array.Length; index++)
+            //{
+            //    Console.WriteLine($"{index}-st Array is `{myClass.array[index]}");
+            //}
+            //myClass.Add(myClass.array, number);
         }
     }
 }
