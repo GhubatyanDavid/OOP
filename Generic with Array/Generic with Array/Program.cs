0c473bd6b24acbb19f740a4aeaf55596ecca18e3
David Ghubatyan <ghubatyandavid@gmail.com>
2021-12-10 13:01:01 +0400

    Array with Generic

---
+++ Generic with Array/Generic with Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Generic_with_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Type the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ArrayClass<int> arrayClass = new ArrayClass<int>();
+            ArrayClass<int> arrayClass1 = new ArrayClass<int>(4, 2, 6, 8);
+            arrayClass.Add(arrayClass.array, number);
+        }
+    }
+}
