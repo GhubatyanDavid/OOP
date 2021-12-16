58fc83d20be6228f32976afbf9ce68e1454de7cb
David Ghubatyan <ghubatyandavid@gmail.com>
2021-12-16 15:25:43 +0400

    ArrayList

---
+++ ArrayList with System Collections/ArrayList with System Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrayList_with_System_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(Convert.ToInt32(Console.ReadLine()));
+            arrayList.Add(Convert.ToInt32(Console.ReadLine()));
+            arrayList.Add((object)Convert.ToInt32(Console.ReadLine()));
+            for (int i = 0; i < arrayList.Count; i++)
+                Console.WriteLine((int)arrayList[i]);
+        }
+    }
+    }
+
