692a2a006dba5ad63c84da93a8302bd663b0538d
David Ghubatyan <ghubatyandavid@gmail.com>
2022-01-05 17:07:59 +0400

    New Zoo :)

--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -7,11 +7,11 @@ namespace Zoo
         static void Main(string[] args)
         {
 
-            Animals animals = new Animals("Aryuc", "Mammal");
-            Animals animals1 = new Animals("Shun", "Mammal");
-            Animals animals2 = new Animals("Kapik","Mammal");
-            Animals animals3 = new Animals("Arciv","Bird");
-            Animals animals4 = new Animals("Moxes","Reptile");
+            Animals animals = new Animals("Tiger", "Mammal");
+            Animals animals1 = new Animals("Cow", "Mammal");
+            Animals animals2 = new Animals("Monkey","Mammal");
+            Animals animals3 = new Animals("Eagle","Bird");
+            Animals animals4 = new Animals("Lizard","Reptile");
             Zoo<Animals> zoo = new Zoo<Animals>(5);
             zoo.AddAnimal(animals, animals1, animals2, animals3, animals4);
             animals.Run(zoo.animal);
