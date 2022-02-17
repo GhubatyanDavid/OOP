0d1a6ee508e9ef3e33129cd4334e75431d534950
David Ghubatyan <ghubatyandavid@gmail.com>
2022-02-17 19:57:01 +0400

    MyDictionary

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey,TValue> : IEnumerable , IEnumerator where TKey :class
+    {
+        Element<TKey,TValue>[] elementArray = null;
+        public MyDictionary(Element<TKey,TValue>[] element)
+        {
+            elementArray = element;
+            for (int i = 0; i < element.Length; i++)
+            {
+                for (int j = 1; j < element.Length; j++)
+                {
+                    if (elementArray[i].key == element[j].key & i != j)
+                    {
+                        throw new Exception("Error");
+                    }
+                    else
+                    {
+                        elementArray[i] = element[i];
+                    }
+                    
+                }
+            }
+        }
+        int  position = 0;
+        public Element<TKey,TValue> this[int index]
+        {
+            get { return elementArray[index]; }
+            set { elementArray[index] = value; }
+        }
+        public bool MoveNext()
+        {
+            if (position < elementArray.Length)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+            
+        }
+        public void Reset()
+        {
+            position = 0;
+        }
+        public object Current
+        {
+            get
+            {
+                return elementArray[position-1];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Используя Visual Studio, создайте проект по шаблону Console Application. 
+            Создайте коллекцию MyDictionary<TKey,TValue>. Реализуйте в простейшем приближении 
+            возможность использования ее экземпляра аналогично экземпляру класса Dictionary<TKey,TValue>. 
+            Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод 
+            добавления элемента, индексатор для получения значения элемента по указанному индексу и свойство 
+            только для чтения для получения общего количества элементов. Реализуйте возможность перебора 
+            элементов коллекции в цикле foreach.*/
+            Element<string,string>[] elementsArray = new Element<string, string>[8];
+            elementsArray[0] = new Element<string, string>("Book", "Girq");
+            elementsArray[1] = new Element<string, string>("Dog", "Shun");
+            elementsArray[2] = new Element<string, string>("Cat", "Katu");
+            elementsArray[3] = new Element<string, string>("Rabbit", "Napastak");
+            elementsArray[4] = new Element<string, string>("Car", "Meqena");
+            elementsArray[5] = new Element<string, string>("Cat", "Katu");
+            elementsArray[6] = new Element<string, string>("Woman", "Kin");
+            elementsArray[7] = new Element<string, string>("Male", "Txamard");
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>(elementsArray);
+            foreach (Element<string,string> element in myDictionary)
+            {
+                Console.WriteLine("English: {0}  Armenian: {1} ", element.key,element.value);
+            }
+
+        }
+
+    }
+}
+
