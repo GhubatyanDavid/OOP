4a36345598df4633077c510b4acdec78ca5cac96
David Ghubatyan <ghubatyandavid@gmail.com>
2022-03-06 19:33:18 +0400

    Prices in shop

---
+++ PricesinShop/PricesinShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PricesinShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] shop = new string[5] { "Gucci", "Addidas", "Toyota Motors", "Ikea", "Puma" };
+            Console.WriteLine("We have only five shops`");
+            for (int i = 0; i < shop.Length; i++)
+            {
+                Console.WriteLine($"Shop name`{shop[i]}");
+            }
+            Console.WriteLine("Please type Shop name `");
+            string shopName = Console.ReadLine();
+            Console.WriteLine("Please type Product name`");
+            string productName = Console.ReadLine();
+            Console.WriteLine("Please add price`");
+            int money = Convert.ToInt32(Console.ReadLine());
+            Price price = new Price(productName,shopName,money);
+            for (int i = 0; i < shop.Length; i++)
+            {
+                if(shop[i] == shopName)
+                {
+                    Console.WriteLine(price.ToString());
+                    break;
+                }
+                else if(i == shop.Length -1 )
+                {
+                    try
+                    {
+                        throw new Exception(); 
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error wrong shop name!!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
