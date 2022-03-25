fa6503fa05cb5a37ea2ae66f5081cf6b915c5f41
David Ghubatyan <ghubatyandavid@gmail.com>
2022-03-25 13:54:40 +0400

    Working with 2 namespaces

---
+++ Namespaces/Namespaces/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace Cars
+{
+    class Car
+    {
+        private string _carModel;
+        private string _carBrand;
+        private int _carYear;
+        public Car(string carModel,string carBrand,int carYear)
+        {
+            _carModel = carModel;
+            _carBrand = carBrand;
+            _carYear = carYear;
+        }
+        public void CarInfo()
+        {
+            System.Console.WriteLine("Car Model`{0},Car Brand`{1},Car Year`{2}",_carModel,_carBrand,_carYear);
+        }
+    }
+}
